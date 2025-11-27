31c84325527e0e140baeee65a0bb41530d74251b
Salma Fahmy <167946753+salma-fahmy@users.noreply.github.com>
2025-11-27 21:07:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# رابط CSV مباشر
+# رابط مباشر للملف
 url = "https://www.dropbox.com/scl/fi/wx0fsu580mfl0kjcaub2f/cleaned_reviews.csv?dl=1"
 
-# قراءة CSV
 try:
-    df = pd.read_csv(url)
+    # قراءة الملف مع التجربة بفاصل تلقائي وحل مشاكل الترميز
+    df = pd.read_csv(url, sep=None, engine='python', encoding='utf-8')
     st.write("أسماء الأعمدة في الملف:")
     st.write(df.columns.tolist())
 except Exception as e:
