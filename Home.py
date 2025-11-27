1c59500fc5a40159d4d2ceba34d874d3e0aacd93
Salma Fahmy <167946753+salma-fahmy@users.noreply.github.com>
2025-11-27 21:05:59 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-# رابط CSV مباشر من Dropbox
+# رابط CSV مباشر
 url = "https://www.dropbox.com/scl/fi/wx0fsu580mfl0kjcaub2f/cleaned_reviews.csv?dl=1"
 
 # قراءة CSV
-df = pd.read_csv(url)
-
-# عرض أسماء الأعمدة
-st.write("أسماء الأعمدة في الملف:")
-st.write(df.columns.tolist())
+try:
+    df = pd.read_csv(url)
+    st.write("أسماء الأعمدة في الملف:")
+    st.write(df.columns.tolist())
+except Exception as e:
+    st.error(f"حدث خطأ أثناء قراءة الملف: {e}")
